c693b697b84ebc294bff589b1e2e8e08e1ae7948
Christian Nwachukwu <christian@Christians-MacBook-Pro.local>
2021-08-21 10:12:24 -0400

    Completed the assignment

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,7 +7,30 @@
   \***********************/
 /***/ (() => {
 
+$(document).ready(function () {
+  $(document).scroll(function () {
+    if ($(document).scrollTop() > 30) {
+      $('.navbar').removeClass('nav-transparent');
+      $('.navbar').addClass('nav-sticky');
+    } else {
+      $('.navbar').removeClass('nav-sticky');
+      $('.navbar').addClass('nav-transparent');
+    }
+  });
+  $('.nav-link').click(function () {
+    var links = $('.nav-link');
+    var current_target = $(this).data('target');
+    $(links).each(function (index, obj) {
+      var target = $(obj).data('target');
 
+      if (current_target === target) {
+        $(obj).addClass('active');
+      } else {
+        $(obj).removeClass('active');
+      }
+    });
+  });
+});
 
 /***/ }),
 
